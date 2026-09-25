4b0ff638120f602eef0d7525b5944d2344a007ef
rendeb-dsb <87785469+rendeb-dsb@users.noreply.github.com>
2026-09-25 10:54:33 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dump-pwa-v17';
+const CACHE_NAME = 'dump-pwa-v18';
 const ASSETS = ['./', './index.html', './manifest.json', './sw.js'];
 
 self.addEventListener('install', event => {
